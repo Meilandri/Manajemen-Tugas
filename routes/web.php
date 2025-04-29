9365ee70c4077d09fc330df60a76dd4cf77a5449
Meilandri Aditya <meilandriaditya@gmail.comgit config --global user.name Meilandri>
2025-04-29 23:05:24 +0700

    Video part 3 Landing page, Login, User

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TugasController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+// Dashboard
+Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard');
+
+// login
+Route::get('login',[AuthController::class,'login'])->name ('login'); 
+
+// user
+Route::get('user',[UserController::class,'index'])->name ('user'); 
+
+// Tugas
+Route::get('tugas',[TugasController::class,'index'])->name ('tugas'); 
